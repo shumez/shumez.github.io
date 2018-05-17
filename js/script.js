4af4704f0c2436a1054858278405f3b502c2341a
shumez <shumelancholie+dev@gmail.com>
2018-05-17 21:24:33 +0900

    change id

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 	$('.mdl-navigation > .my-nav-home').click(function(){
 		$('.section-active').removeClass('section-active');
 
-		$('.my-section#section-home').addClass('section-active');
+		$('.my-section#home').addClass('section-active');
 
 		$('.my-nav-active').removeClass('my-nav-active');
 		$('.my-nav-home').addClass('my-nav-active');
@@ -19,7 +19,7 @@ $(document).ready(function(){
 	$('.mdl-navigation > .my-nav-slide').click(function(){
 		$('.section-active').removeClass('section-active');
 
-		$('.my-section#section-slide').addClass('section-active');
+		$('.my-section#slide').addClass('section-active');
 
 		$('.my-nav-active').removeClass('my-nav-active');
 		$('.my-nav-slide').addClass('my-nav-active');
@@ -29,7 +29,7 @@ $(document).ready(function(){
 	$('.mdl-navigation > .my-nav-docs').click(function(){
 		$('.section-active').removeClass('section-active');
 
-		$('.my-section#section-docs').addClass('section-active');
+		$('.my-section#docs').addClass('section-active');
 
 		$('.my-nav-active').removeClass('my-nav-active');
 		$('.my-nav-docs').addClass('my-nav-active');
@@ -39,7 +39,7 @@ $(document).ready(function(){
 	$('.mdl-navigation > .my-nav-proj').click(function(){
 		$('.section-active').removeClass('section-active');
 
-		$('.my-section#section-proj').addClass('section-active');
+		$('.my-section#proj').addClass('section-active');
 
 		$('.my-nav-active').removeClass('my-nav-active');
 		$('.my-nav-proj').addClass('my-nav-active');
