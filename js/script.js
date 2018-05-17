3693e8f252b82bb6c4392042980c9c511569f94c
shumez <shumelancholie+dev@gmail.com>
2018-05-17 20:11:36 +0900

    nav class

--- js/script.js
+++ js/script.js
@@ -8,7 +8,10 @@ $('document').ready(function(){
 	$('.mdl-navigation > .my-nav-home').click(function(){
 		$('.section-active').removeClass('section-active');
 
-		$('.my-section#section-home').addSection('section-active');
+		$('.my-section#section-home').addClass('section-active');
+
+		$('.my-nav-active').removeClass('my-nav-active');
+		$('.my-nav-home').addClass('my-nav-active');
 
 	});
 
@@ -16,23 +19,29 @@ $('document').ready(function(){
 	$('.mdl-navigation > .my-nav-slide').click(function(){
 		$('.section-active').removeClass('section-active');
 
-		$('.my-section#section-slide').addSection('section-active');
+		$('.my-section#section-slide').addClass('section-active');
 
+		$('.my-nav-active').removeClass('my-nav-active');
+		$('.my-nav-slide').addClass('my-nav-active');
 	});
 
 
 	$('.mdl-navigation > .my-nav-docs').click(function(){
 		$('.section-active').removeClass('section-active');
 
-		$('.my-section#section-docs').addSection('section-active');
+		$('.my-section#section-docs').addClass('section-active');
 
+		$('.my-nav-active').removeClass('my-nav-active');
+		$('.my-nav-docs').addClass('my-nav-active');
 	});
 
 
 	$('.mdl-navigation > .my-nav-proj').click(function(){
 		$('.section-active').removeClass('section-active');
 
-		$('.my-section#section-proj').addSection('section-active');
+		$('.my-section#section-proj').addClass('section-active');
 
+		$('.my-nav-active').removeClass('my-nav-active');
+		$('.my-nav-proj').addClass('my-nav-active');
 	});
 };
