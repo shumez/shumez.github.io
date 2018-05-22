316263038dd589b639e5fdd2e744d563db059587
shumez <shumelancholie+dev@gmail.com>
2018-05-22 10:47:36 +0900

    add demo/test/

---
+++ demo/test/js/script.js
@@ -0,0 +1,58 @@
+// script.js
+$(document).ready(function(){
+
+
+	// $('.my-section').hide();
+	// $('.section-active').show();
+
+	$('.mdl-navigation > .my-nav-home').click(function(){
+		$('.section-active').removeClass('section-active');
+
+		$('.my-section#home').addClass('section-active');
+
+		$('.my-nav-active').removeClass('my-nav-active');
+		$('.my-nav-home').addClass('my-nav-active');
+
+		$('.my-title-active').removeClass('my-title-active');
+		$('.my-title-home').addClass('my-title-active');
+	});
+
+
+	$('.mdl-navigation > .my-nav-slide').click(function(){
+		$('.section-active').removeClass('section-active');
+
+		$('.my-section#slide').addClass('section-active');
+
+		$('.my-nav-active').removeClass('my-nav-active');
+		$('.my-nav-slide').addClass('my-nav-active');
+
+		$('.my-title-active').removeClass('my-title-active');
+		$('.my-title-slide').addClass('my-title-active');
+	});
+
+
+	$('.mdl-navigation > .my-nav-docs').click(function(){
+		$('.section-active').removeClass('section-active');
+
+		$('.my-section#docs').addClass('section-active');
+
+		$('.my-nav-active').removeClass('my-nav-active');
+		$('.my-nav-docs').addClass('my-nav-active');
+
+		$('.my-title-active').removeClass('my-title-active');
+		$('.my-title-docs').addClass('my-title-active');
+	});
+
+
+	$('.mdl-navigation > .my-nav-proj').click(function(){
+		$('.section-active').removeClass('section-active');
+
+		$('.my-section#proj').addClass('section-active');
+
+		$('.my-nav-active').removeClass('my-nav-active');
+		$('.my-nav-proj').addClass('my-nav-active');
+
+		$('.my-title-active').removeClass('my-title-active');
+		$('.my-title-proj').addClass('my-title-active');
+	});
+});
